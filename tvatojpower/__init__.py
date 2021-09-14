d9e8e3e56e4daabac6d3ff439ac21e2e0e3719e5
Jan Tünnermann <jan.tuennermann@uni-marburg.de>
2021-09-14 13:01:59 +0200

    Create __init__.py

---
+++ tvatojpower/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2020 Jan Tünnermann. All rights reserved.
+# This work is licensed under the terms of the MIT license.  
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+from tvatojpower import (
+   hierarchical_model_noncentered
+   sim_and_fit
+)
